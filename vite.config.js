e788d7b2642487866ddb2d04891ec53212dbc141
isabbb <124369384+isabbb@users.noreply.github.com>
2025-11-11 11:03:13 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['acueducto-rk5c.onrender.com'],
+    port: process.env.PORT || 4173,
+    host: '0.0.0.0'
+  }
 })
